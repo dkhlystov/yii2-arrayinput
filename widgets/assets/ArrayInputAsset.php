ec45f547028e07640d09714c91ed04b3dad4134a
Dmitry Khlystov <dkhlystov@gmail.com>
2018-08-30 20:10:30 +1000

    Moving support + minify

--- widgets/assets/ArrayInputAsset.php
+++ widgets/assets/ArrayInputAsset.php
@@ -7,24 +7,24 @@ use yii\web\AssetBundle;
 class ArrayInputAsset extends AssetBundle
 {
 
-	public $js = [
-		'array-input.js',
-	];
+    public $js = [
+        'array-input' . (YII_DEBUG ? '' : '.min') . '.js',
+    ];
 
-	public $css = [
-		'array-input.css',
-	];
+    public $css = [
+        'array-input' . (YII_DEBUG ? '' : '.min') . '.css',
+    ];
 
-	public $depends = [
-		'yii\web\JqueryAsset',
-	];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
 
-	/**
-	 * @inheritdoc
-	 */
-	public function init()
-	{
-		$this->sourcePath = __DIR__ . '/array-input';
-	}
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->sourcePath = __DIR__ . '/array-input';
+    }
 
 }
--- widgets/assets/array-input/array-input.js
+++ widgets/assets/array-input/array-input.js
@@ -1,43 +1,52 @@
-$(function() {
+$(document).on('click', '.array-input > button', itemAddClick);
+$(document).on('click', '.array-input .item-remove', itemRemoveClick);
+$(document).on('moving-column.moved', '.array-input-table tr', itemMove);
 
-	// https://github.com/yiisoft/yii2/issues/13352
-	$('.array-input > table > tbody > tr > td > div.empty').closest('tr').remove();
+function itemAddClick(e)
+{
+	itemAdd($(this).prev());
+};
 
-	$(document).on('click', '.array-input > button', itemAddClick);
-	$(document).on('click', '.array-input .item-remove', itemRemoveClick);
+function itemRemoveClick(e)
+{
+	e.preventDefault();
+	$(this).closest('tr').remove();
+};
 
-	function itemAddClick(e)
-	{
-		itemAdd($(this).prev());
-	}
+function itemMove(e)
+{
+	$(this).parent().find('tr').each(function() {
+		var $row = $(this);
+		itemIndex($row, $row.index());
+	});
+};
 
-	function itemRemoveClick(e)
-	{
-		e.preventDefault();
-		$(this).closest('tr').remove();
-	}
+function itemIndex($row, idx)
+{
+	$row.find('input, textarea, select').each(function() {
+		this.name = this.name.replace(/\[\d+\]/, '[' + idx + ']');
+	});
+};
 
-	function itemAdd($table)
-	{
-		var $row = $($table.parent().data('arrayInputTemplate'));
+function itemAdd($table)
+{
+	var $row = $($table.parent().data('arrayInputTemplate'));
 
-		//index
-		var idx = -1;
-		$table.find('input, textarea, select').each(function() {
-			var m = this.name.match(/.+\[(\d+)\]\[[^\]]+\]/),
-				i = m === null ? -1 : parseInt(m[1]);
+	//index
+	var idx = -1;
+	$table.find('input, textarea, select').each(function() {
+		var m = this.name.match(/.+\[(\d+)\]\[[^\]]+\]/),
+			i = m === null ? -1 : parseInt(m[1]);
 
-			if (i > idx) idx = i;
-		});
-		idx++;
+		if (i > idx) {
+			idx = i;
+		};
+	});
+	idx++;
 
-		//properties
-		$row.removeClass('hidden');
-		$row.find('input, textarea, select').prop('disabled', false).each(function() {
-			this.name = this.name.replace(/\[0\]/, '[' + idx + ']');
-		});
+	//properties
+	$row.removeClass('hidden');
+	itemIndex($row, idx);
 
-		$table.find('tbody').append($row);
-	}
-
-});
+	$table.find('tbody').append($row);
+};
